735cd0d6da53f10db3778332aed38d01419ef2ad
AndroidLevi <lhuszar@gmail.com>
2014-12-10 21:13:36 +0100

    1.06 Get a reference to the ListView, and attach this adapter to it.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -87,6 +88,11 @@ public class MainActivity extends ActionBarActivity {
                             );
 
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            //// Get a reference to the ListView, and attach this adapter to it.
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(forecastAdapter);
+
             return rootView;
         }
     }

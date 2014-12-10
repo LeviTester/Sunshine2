4bab052de926900e3302c0987773ccff5347048e
AndroidLevi <lhuszar@gmail.com>
2014-12-10 20:44:13 +0100

    1.03

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -59,6 +59,11 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
+            //String [] data =
+
+
+
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
